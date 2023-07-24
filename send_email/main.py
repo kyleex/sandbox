9295b43e03b9f78af5653eb0a698e735d73bf32d
kyleex <kyleex@skiff.com>
2023-07-24 22:53:31 +0100

    🎈 perf(ux): add prompt msg

--- send_email/main.py
+++ send_email/main.py
@@ -12,7 +12,8 @@ email_sender = os.getenv('email_sender')
 email_password = os.getenv('email_password')
 
 # mail
-email_receiver = "antwill34@icloud.com"
+email_receiver = ["antwill34@icloud.com", 'nistitogni@gufum.com']
+email_cc = []
 subject = "Test - Python script"
 body = """
     It's a test    
@@ -20,18 +21,21 @@ body = """
 em = EmailMessage()
 em['From'] = email_sender
 em['To'] = email_receiver
+em['Cc'] = email_cc
 em['Subject'] = subject
 em.set_content(body)
 
 # Add SSL (layer of security)
 context = ssl.create_default_context()
 
-# Log in and send the email
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-    smtp.login(email_sender, email_password)
-    smtp.sendmail(email_sender, email_receiver, em.as_string())
 
 
 
-
+try :
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+    print("Email sent successfully !")
+except smtplib.SMTPException :
+    print("Failed to send email")
 
