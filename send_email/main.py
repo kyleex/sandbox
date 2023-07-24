211e7e5835446589aef5ea24bc5dafdff540687d
kyleex <kyleex@skiff.com>
2023-07-24 23:10:43 +0100

    ✨ feat(send_email): add CC

--- send_email/main.py
+++ send_email/main.py
@@ -3,8 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #email pkg
-import smtplib
-import ssl
+import smtplib, ssl
 from email.message import EmailMessage
 
 #gmail log in
@@ -12,29 +11,26 @@ email_sender = os.getenv('email_sender')
 email_password = os.getenv('email_password')
 
 # mail
-email_receiver = ["antwill34@icloud.com", 'nistitogni@gufum.com']
-email_cc = []
+email_receiver = ["antwill34@icloud.com"]
+email_cc = ["antwill344@gmail.com"]
 subject = "Test - Python script"
 body = """
     It's a test    
 """
-em = EmailMessage()
-em['From'] = email_sender
-em['To'] = email_receiver
-em['Cc'] = email_cc
-em['Subject'] = subject
-em.set_content(body)
+msg = EmailMessage()
+msg['From'] = email_sender
+msg['To'] = email_receiver
+msg['Cc'] = email_cc
+msg['Subject'] = subject
+msg.set_content(body)
 
 # Add SSL (layer of security)
 context = ssl.create_default_context()
 
-
-
-
 try :
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
+        smtp.send_message(msg)
     print("Email sent successfully !")
 except smtplib.SMTPException :
     print("Failed to send email")
