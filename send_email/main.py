3d0b2581c113fabbba4702ee46f1d6f611e0bb5b
kyleex <kyleex@skiff.com>
2023-07-24 23:53:47 +0100

    ✨ feat(send_email): Attach file
    Insert the path of the file to attach in a email

--- send_email/main.py
+++ send_email/main.py
@@ -3,26 +3,54 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #email pkg
-import smtplib, ssl
+import email, smtplib, ssl
 from email.message import EmailMessage
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 #gmail log in
 email_sender = os.getenv('email_sender')
 email_password = os.getenv('email_password')
 
 # mail
-email_receiver = ["antwill34@icloud.com"]
-email_cc = ["antwill344@gmail.com"]
+email_receiver = "here"
+email_cc = "here"
 subject = "Test - Python script"
 body = """
     It's a test    
 """
-msg = EmailMessage()
+msg = MIMEMultipart()
 msg['From'] = email_sender
 msg['To'] = email_receiver
 msg['Cc'] = email_cc
 msg['Subject'] = subject
-msg.set_content(body)
+
+# Add body to email
+msg.attach(MIMEText(body, "plain"))
+
+filename = 'path_file'  # In same directory as script
+
+# Open PDF file in binary mode
+with open(filename, "r") as attachment:
+    # Add file as application/octet-stream
+    # Email client can usually download this automatically as attachment
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+# Encode file in ASCII characters to send by email    
+encoders.encode_base64(part)
+
+# Add header as key/value pair to attachment part
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filename}",
+)
+
+# Add attachment to message and convert message to string
+msg.attach(part)
+text = msg.as_string()
 
 # Add SSL (layer of security)
 context = ssl.create_default_context()
